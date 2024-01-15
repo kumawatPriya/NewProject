6bf32266cbdeee19507ff9074839fc555b2a9edd
kumawatPriya <priyakumawat9868@gmail.com>
2024-01-14 20:24:43 -0800

    first commit

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -3,8 +3,9 @@ import React from "react";
 function Home(){
     return(
         <>
-        
+        <div className="heading">
         <h1>Hey this is home Page</h1>
+        </div>
         </>
     )
 }
